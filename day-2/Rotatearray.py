aa3c2c661d661ff5d46f3bcd1256a2dd7cd8bb13
Harsh Bhatt <98325507+Harsh7520@users.noreply.github.com>
2022-04-12 12:30:35 +0530

    Create Rotatearray.py

---
+++ day-2/Rotatearray.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+def rotate( arr, n):
+    rem = arr.pop(n-1)
+    arr.insert(0,rem)
+    return arr
+    
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        rotate(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
